008d699c880126570be57e94b2b4b8863f10d5bf
workplay <pkuwsh@163.com>
2020-06-02 22:01:22 +0800

    Record and encode screen in a foreground service.

--- app/src/main/java/com/example/foregroundservicedemo/ForegroundService.kt
+++ app/src/main/java/com/example/foregroundservicedemo/ForegroundService.kt
@@ -2,20 +2,61 @@ package com.example.foregroundservicedemo
 
 import android.R
 import android.app.*
+import android.content.Context
 import android.content.Intent
-import android.os.Build
-import android.os.IBinder
+import android.hardware.display.DisplayManager
+import android.media.MediaCodec
+import android.media.MediaCodecInfo
+import android.media.MediaFormat
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.os.*
+import android.util.Log
+import android.view.Surface
 import androidx.core.app.NotificationCompat
+import java.io.IOException
+import java.nio.ByteBuffer
 
 
 class ForegroundService : Service() {
 
-    val CHANNEL_ID = "ForegroundServiceChannel"
+    private var mBufferInfo: MediaCodec.BufferInfo? = null
+    private var workHanlder: Handler? = null
 
+    private var mMediaProjection: MediaProjection? = null
+
+    private var mResultCode = 0
+    private var mResultData: Intent? = null
+
+    private fun startScreenCapture() {
+        val mediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        if (mMediaProjection == null) {
+            mMediaProjection = mediaProjectionManager.getMediaProjection(mResultCode, mResultData!!)
+        }
+        val surface = createSurface()
+        mMediaProjection!!.createVirtualDisplay("ScreenCapture",
+            width, height, 1,
+            DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC,
+            surface, null, null
+        )
+    }
+
+    // TODO: Read paramters from device.
+    private val width = 480
+    private val height = 720
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val CHANNEL_ID = "ForegroundServiceChannel"  // Notification Channel ID.
         val input = intent.getStringExtra("inputExtra")
-        createNotificationChannel()
+        mResultCode = intent.getIntExtra("code", -1)
+        mResultData = intent.getParcelableExtra<Intent>("data")
+
+        if (mResultCode == 0 || mResultData == null) {
+            Log.e("ForgroundService", "Error: Screen record permission is rejected by user")
+        }
+
+        createEncoderThread()
+        createNotificationChannel(CHANNEL_ID)
         val notificationIntent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(
             this,
@@ -27,18 +68,16 @@ class ForegroundService : Service() {
             .setSmallIcon(R.drawable.ic_dialog_alert)
             .setContentIntent(pendingIntent)
             .build()
+        // Create a notification on task bar.
         startForeground(1, notification)
-        //do heavy work on a background thread
-        //stopSelf();
-        return START_NOT_STICKY
+
+        startScreenCapture()
+
+        return START_STICKY
     }
 
 
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
-    }
-
-    private fun createNotificationChannel() {
+    private fun createNotificationChannel(CHANNEL_ID: String) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val serviceChannel = NotificationChannel(
                 CHANNEL_ID,
@@ -52,5 +91,122 @@ class ForegroundService : Service() {
         }
     }
 
+
+    class FrameCallback {
+        val LOG_TAG: String = "FrameCallback."
+
+        fun render(
+            info: MediaCodec.BufferInfo?,
+            outputBuffer: ByteBuffer?
+        ) {
+            Log.d(LOG_TAG, info.toString())
+            Log.d(LOG_TAG,outputBuffer.toString())
+
+        }
+
+        fun formatChange(mediaFormat: MediaFormat?) {
+            Log.d("Shiheng", "Format changed.")
+        }
+    }
+
+    private fun createSurface(): Surface? {
+        // Parameters and constants
+        val MIME_TYPE = "video/avc" // H.264 Advanced Video Coding
+        val FRAME_RATE = 15 // 30fps
+        val IFRAME_INTERVAL = 5 // 5 seconds between I-frames
+        val BIT_RATE = 800000 // 5 seconds between I-frames
+
+
+        mBufferInfo = MediaCodec.BufferInfo()
+        val format = MediaFormat.createVideoFormat(MIME_TYPE, width, height)
+        format.setInteger(
+            MediaFormat.KEY_COLOR_FORMAT,
+            MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface
+        )
+        format.setInteger(MediaFormat.KEY_BIT_RATE, BIT_RATE)
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE)
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL)
+
+        format.setInteger(
+            MediaFormat.KEY_COLOR_FORMAT,
+            MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface
+        )
+        format.setInteger(MediaFormat.KEY_BIT_RATE, BIT_RATE)
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE)
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL)
+
+        try {
+            val mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
+            mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+            val mInputSurface = mEncoder.createInputSurface();
+            mEncoder.start();
+
+            workHanlder!!.postDelayed({
+                doExtract(mEncoder, FrameCallback())
+            }, 1000)
+            return mInputSurface!!
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    private fun createEncoderThread() {
+        val encoder = HandlerThread("Encoder")
+        encoder.start()
+        val looper: Looper = encoder.looper
+        workHanlder = Handler(looper)
+    }
+
+
+    private fun doExtract(
+        encoder: MediaCodec,
+        frameCallback: FrameCallback?
+    ) {
+        val LOG_TAG = "Foreground Service."
+        val VERBOSE = false
+        val TIMEOUT_USEC = 10000
+        var outputDone = false
+        while (!outputDone) {
+            val decoderStatus = encoder.dequeueOutputBuffer(mBufferInfo!!, TIMEOUT_USEC.toLong())
+            if (decoderStatus == MediaCodec.INFO_TRY_AGAIN_LATER) {
+                // TODO: Handle this case.
+
+            } else if (decoderStatus == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                val newFormat = encoder.outputFormat
+                if (VERBOSE) Log.d(
+                    LOG_TAG,
+                    "decoder output format changed: $newFormat"
+                )
+                frameCallback?.formatChange(newFormat)
+            } else if (decoderStatus < 0) {
+                throw RuntimeException(
+                    "unexpected result from decoder.dequeueOutputBuffer: " +
+                            decoderStatus
+                )
+            } else { // decoderStatus >= 0
+                if (VERBOSE) Log.d(
+                    LOG_TAG, "surface decoder given buffer " + decoderStatus +
+                            " (size=" + mBufferInfo!!.size + ")"
+                )
+                if (mBufferInfo!!.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
+                    if (VERBOSE) Log.d(LOG_TAG, "output EOS")
+                    outputDone = true
+                }
+                val doRender = mBufferInfo!!.size != 0
+                if (doRender && frameCallback != null) {
+                    val outputBuffer: ByteBuffer? = encoder.getOutputBuffer(decoderStatus)
+                    frameCallback.render(mBufferInfo, outputBuffer)
+                }
+                encoder.releaseOutputBuffer(decoderStatus, doRender)
+            }
+        }
+    }
+
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
 
 }
--- app/src/main/java/com/example/foregroundservicedemo/MainActivity.kt
+++ app/src/main/java/com/example/foregroundservicedemo/MainActivity.kt
@@ -1,39 +1,64 @@
 package com.example.foregroundservicedemo
 
+import android.content.Context
 import android.content.Intent
+import android.media.projection.MediaProjectionManager
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
-
+    // Buttons on GUI
     private lateinit var btnStartService: Button
     private lateinit var btnStopService: Button
+    // Request Code to get Screen Record Permission
+    private val REQUEST_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnStartService = findViewById(R.id.buttonStartService)
         btnStopService = findViewById(R.id.buttonStopService)
         btnStartService.setOnClickListener {
-            startService()
-
+            // Apply for Screen Record Service.
+            val mMediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+            startActivityForResult(
+                mMediaProjectionManager.createScreenCaptureIntent(),
+                REQUEST_CAPTURE
+            )
         }
+
         btnStopService.setOnClickListener {
                 stopService()
         }
     }
 
-    private fun startService() {
+    private fun startService(resultCode: Int, data: Intent?) {
         val serviceIntent = Intent(this, ForegroundService::class.java)
-        serviceIntent.putExtra("inputExtra", "Foreground Service Example in Android")
+        serviceIntent.putExtra("inputExtra", "Recording Screen.")
+        serviceIntent.putExtra("code", resultCode)
+        serviceIntent.putExtra("data", data)
         ContextCompat.startForegroundService(this, serviceIntent)
     }
 
     private fun stopService() {
         val serviceIntent = Intent(this, ForegroundService::class.java)
         stopService(serviceIntent)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                startService(resultCode, data)
+                Toast.makeText(this, "Capture Screen Service Starts.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Capture Screen Service Fails to start.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
